42fa969e241ad539cd0568b1ddfbeb07256eaedd
Adam-Steele <adamsteele@hotmail.co.uk>
2021-04-27 20:13:58 +0100

    env security

--- server/index.js
+++ server/index.js
@@ -4,9 +4,12 @@ import mongoose from 'mongoose';
 import cors from 'cors';
 
 import recipeRoutes from "./routes/recipes.js";
+import dotenv from "dotenv";
 
 
 const app = express();
+dotenv.config();
+
 app.use(cors());
 app.use(bodyParser.json({limit: "30mb", extended:true}));
 app.use(bodyParser.urlencoded({limit: "30mb", extended:true}));
@@ -14,11 +17,11 @@ app.use(bodyParser.urlencoded({limit: "30mb", extended:true}));
 
 app.use('/recipes', recipeRoutes)
 
-const CONNECTION_URL = 'mongodb+srv://Adam-Steele:Baldwin0118@cluster0.oqqan.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
+//const CONNECTION_URL = 'mongodb+srv://Adam-Steele:Baldwin0118@cluster0.oqqan.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
 
 const PORT = process.env.PORT || 5000;
 
-mongoose.connect(CONNECTION_URL,{useNewUrlParser:true, useUnifiedTopology:true})
+mongoose.connect(process.env.CONNECTION_URL,{useNewUrlParser:true, useUnifiedTopology:true})
     .then(()=> app.listen(PORT,()=>console.log(`Server running on port: ${PORT}`)))
     .catch((error)=>console.log(error.message));
 
